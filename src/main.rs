9c59bbf80ae950d295751f009cfa4015d60f2354
Tidle <tidle@users.noreply.github.com>
2020-02-01 16:55:45 -0800

    added short_word() function

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,27 @@ fn main() {
         Roll5(v) => println!("{}", v[0])
     }
 }
+
+fn short_word(dice: [u8; 4]) -> std::io::Result<String> {
+    use std::{fs, io};
+    use std::io::BufRead;
+
+    let matchstr = format!("{}{}{}{}", dice[0], dice[1], dice[2], dice[3]);
+
+    let wordlist = fs::File::open("list4.txt")?;
+    let wordlist = io::BufReader::new(wordlist);
+    let mut result = String::new();
+
+    for line in wordlist.lines() {
+        let line = line?;
+        if let Some(0) = line.find(&matchstr) {
+            result = String::from( match line.split(" ").nth(1) {
+                Some(s) => s,
+                None => continue,
+            });
+            break;
+        }
+    }
+
+    Ok(result)
+}
